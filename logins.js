c98b6891e92bd37a84adfb705e4f6d9fca411429
Fenille <rpfenille@gmail.com>
2025-12-01 21:06:54 -0300

    Add files via upload

---
+++ logins.js
@@ -0,0 +1,9 @@
+// Arquivo separado apenas com logins e senhas locais
+// IMPORTANTE: isto não é autenticação segura, é apenas controle interno da página.
+
+export const usuariosAutorizados = [
+  { login: "Fenille",  senha: "140965" },
+  { login: "Previato", senha: "132800" },
+  { login: "Gleison",  senha: "133069" },
+  { login: "Melo",     senha: "991315" }
+];
