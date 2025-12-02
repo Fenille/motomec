b93be50ced6a18755267b23160d24ec68e5372a3
Fenille <rpfenille@gmail.com>
2025-12-02 14:55:28 -0300

    Add files via upload

--- logins.js
+++ logins.js
@@ -1,7 +1,61 @@
-// logins.js - controle simples de usuários (apenas lado cliente)
+// logins.js - controle de usuários para acesso aos módulos de frota
+// Cada usuário tem uma lista de módulos autorizados em `modulos`.
+// Chaves de módulo usadas no index:
+//  - "1CIA","2CIA","3CIA","4CIA","5CIA","FT","EM","BTL"
+
 export const usuariosAutorizados = [
-  { login: "Fenille",  senha: "140965" },
-  { login: "Previato", senha: "132800" },
-  { login: "Gleison",  senha: "133069" },
-  { login: "Melo",     senha: "991315" }
+  // Acesso total (para testes / administração)
+  {
+    login: "Fenille",
+    senha: "140965",
+    modulos: ["1CIA","2CIA","3CIA","4CIA","5CIA","FT","EM","BTL"]
+  },
+
+  // Cias individuais
+  {
+    login: "1cia",
+    senha: "1cia123",
+    modulos: ["1CIA"]
+  },
+  {
+    login: "2cia",
+    senha: "2cia123",
+    modulos: ["2CIA"]
+  },
+  {
+    login: "3cia",
+    senha: "3cia123",
+    modulos: ["3CIA"]
+  },
+  {
+    login: "4cia",
+    senha: "4cia123",
+    modulos: ["4CIA"]
+  },
+  {
+    login: "5cia",
+    senha: "5cia123",
+    modulos: ["5CIA"]
+  },
+
+  // Força Tática
+  {
+    login: "ft",
+    senha: "ft123",
+    modulos: ["FT"]
+  },
+
+  // Estado Maior
+  {
+    login: "em",
+    senha: "em123",
+    modulos: ["EM"]
+  },
+
+  // Batalhão (pode ver todos os painéis, inclusive EM)
+  {
+    login: "btl",
+    senha: "btl123",
+    modulos: ["1CIA","2CIA","3CIA","4CIA","5CIA","FT","EM","BTL"]
+  }
 ];
